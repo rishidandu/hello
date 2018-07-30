8ba364be9d2c1221fce5bde335369368ef00dbc5
Rishi Dandu <rkd160030@cslinux1.utdallas.edu>
2018-07-30 11:41:55 -0500

    First commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+ void main()
+ {
+     int i;
+         for(i = 0; i < 10; ++i)
+	     {
+	             #define hell "Hell! World!"
+		             printf( hell " %d\n", i);
+			         }
+				  
+				      printf( hell " out of scope\n");
+				      }
